03f0fb96041cbdf88f2f775d19b65e52961c770c
vg2425 <vg2425@columbia.edu>
2021-12-01 22:42:36 -0500

    RecipeButtons

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import styled from "styled-components";
 
-const recipeButton = styled.button`
+const RecipeButton = styled.button`
   position: relative; 
   top: ${(props) => props.position[0]};
   left: ${(props) => props.position[1]};
   transform: rotate(${(props) => props.rotation});
 `;
 
-const Button = ({setUrl, name, position, rotation, url}) => {
-  return (<button onClick={()=>{setUrl("hello")}}></button>);
+const Button = ({setUrl, position, rotation, url}) => {
+  return (
+    <RecipeButton position={position} rotation={rotation} onClick={()=>{setUrl(url)}}></RecipeButton>);
 };
 
 export default Button;
---
+++ src/data/Buttons.js
@@ -0,0 +1,11 @@
+export const Buttons = [{
+    name: "Exhausted",
+    position: ["10%", "15%"],
+    rotation: "5%",
+    url: ".com"
+},
+{
+    
+}
+
+]
